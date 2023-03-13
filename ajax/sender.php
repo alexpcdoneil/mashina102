14cd355cc128c695b118d7d963f6b68c50fad17a
Afanasev Alexander <afanasev.a@openregion.info>
2023-03-13 19:19:56 +0500

    mail sender

--- ajax/sender.php
+++ ajax/sender.php
@@ -1,33 +1,31 @@
 <?php
+    $name = $_POST['name'];
+    $carModel = $_POST['carModel'];
+    $creationYear = $_POST['creationYear'];
+    $carMileage = isset($_POST['carMileage']) ?? '-';
+    $gearbox = $_POST['gearbox'];
+    $problem = $_POST['problem'];
+    $carLocation = $_POST['carLocation'];
+    $expectedAmount = isset($_POST['expectedAmount']) ?? '-';
+    $phone = $_POST['phone'];
 
-$name = $_POST['name'];
-$carModel = $_POST['carModel'];
-$creationYear = $_POST['creationYear'];
-$carMileage = isset($_POST['carMileage']) ?? '-';
-$gearbox = $_POST['gearbox'];
-$problem = $_POST['problem'];
-$carLocation = $_POST['carLocation'];
-$expectedAmount = isset($_POST['expectedAmount']) ?? '-';
-$phone = $_POST['phone'];
+    $subject = "Письмо от клиента с сайта"; 
 
-$subject = "Письмо от клиента с сайта"; 
+    $to = "sqwertygfd@yandex.ru";
 
-$to = "sqwertygfd@yandex.ru";
+    $msg = "Имя клиента: $name\n".
+        "Марка и модель авто: $carModel\n".
+        "Год выпуска: $creationYear\n".
+        "Пробег: $carMileage\n".
+        "Коробка: $gearbox\n".
+        "проблема: $problem\n".
+        "Где находится авто: $carLocation\n".
+        "Желаемая сумма: $expectedAmount\n".
+        "Телефон для связи: $phone";
 
-$msg = "Имя клиента: $name\n".
-    "Марка и модель авто: $carModel\n".
-    "Год выпуска: $creationYear\n".
-    "Пробег: $carMileage\n".
-    "Коробка: $gearbox\n".
-    "проблема: $problem\n".
-    "Где находится авто: $carLocation\n".
-    "Желаемая сумма: $expectedAmount\n".
-    "Телефон для связи: $phone";
-
-if (mail($to, $subject, $msg)) {
-    echo "Сообщение отправлено!";
-} else {
-    echo "Произошла ошибка при отправке сообщения.";
-}
-
+    if (mail($to, $subject, $msg)) {
+        echo "Сообщение отправлено!";
+    } else {
+        echo "Произошла ошибка при отправке сообщения.";
+    }
 ?>
